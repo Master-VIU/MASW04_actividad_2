4d0d4320ae024614e7c725fd8f45a296e198b3e5
Mayerly Bustos <bustosrobayom@gmail.com>
2023-02-20 21:02:18 -0500

    migration creation

---
+++ database/migrations/2023_02_21_014545_booking.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('booking', function (Blueprint $table) {
+            $table->id_resenya();
+            $table->string('puntuacion', 50);
+            $table->string('opinion', 250);
+            $table->date('fecha');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('booking');
+    }
+};
---
+++ database/migrations/2023_02_21_015558_staff.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->id_usuario();
+            $table->string('dni', 9);
+            $table->string('cif', 9);
+          //  $table->rol('rol');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff');
+    }
+};
