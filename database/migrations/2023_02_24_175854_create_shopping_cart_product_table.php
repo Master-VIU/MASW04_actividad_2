3708e3d104df20f9761fe2309103c21686f5faa7
alopezarias <alopea08@estudiantes.unileon.es>
2023-02-24 19:39:02 +0100

    added shopping cart product

---
+++ database/migrations/2023_02_24_175854_create_shopping_cart_product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shopping_cart_product', function (Blueprint $table) {
+            $table->id('shopping_cart_product_id');
+            $table->foreignId('shopping_cart_id')->references('shopping_cart_id')->on('shopping_cart')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('product_id')->references('product_id')->on('product')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('quantity')->nullable(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shopping_cart_product');
+    }
+};
