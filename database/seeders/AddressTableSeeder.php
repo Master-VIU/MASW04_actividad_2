9505c87d139c63fbad2a2799fff344f587f65d33
Mayerly Bustos <bustosrobayom@gmail.com>
2023-02-25 08:19:25 -0500

    code formatting

--- database/seeders/AddressTableSeeder.php
+++ database/seeders/AddressTableSeeder.php
@@ -17,15 +17,15 @@ class AddressTableSeeder extends Seeder
      */
     public function run()
     {
-        $arrays = range(0,10);
-            foreach ($arrays as $valor) {
-                DB::table('address')->insert([
-                    'street' => Str::random(200),
-                    'street_number' => rand(1, 999),
-                    'city' => Str::random(100),
-                    'postal_code' => rand(1, 999999),
-                    'created_at' => Date::now(),
-                    'updated_at' => Date::now()
+        $arrays = range(0, 10);
+        foreach ($arrays as $valor) {
+            DB::table('address')->insert([
+                'street' => Str::random(200),
+                'street_number' => rand(1, 999),
+                'city' => Str::random(100),
+                'postal_code' => rand(1, 999999),
+                'created_at' => Date::now(),
+                'updated_at' => Date::now()
             ]);
         }
     }
