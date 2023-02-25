26ce8bda9fa845059b792b91e65c0d85599dd9a6
Mayerly Bustos <bustosrobayom@gmail.com>
2023-02-25 13:33:45 -0500

    Product created model

---
+++ app/Models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $table = "product";
+    protected $primaryKey = "product_id";
+
+    protected $fillable = [
+        'product_id',
+        'categorproduct_idy_id',
+        'category_id',
+        'name',
+        'description',
+        'price',
+        'properties',
+        'stock',
+    ];
+}
