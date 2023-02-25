88ea1cb95f577f9c7cd78b15d3c3fd7638cf5459
Mayerly Bustos <bustosrobayom@gmail.com>
2023-02-25 13:32:30 -0500

    created model ClientAddress

---
+++ app/Models/ClientAddress.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClientAddress extends Model
+{
+    use HasFactory;
+
+    protected $table = "client_address";
+    protected $primaryKey = "client_address_id";
+
+    protected $fillable = [
+        'client_address_id',
+        'client_id',
+        'address_id',
+        'client_id',
+    ];
+}
