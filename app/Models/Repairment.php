2770d62f1c72b2936fbd7798fb6d0d9eed127cfd
Mayerly Bustos <bustosrobayom@gmail.com>
2023-02-25 13:34:30 -0500

    Repairment created model

---
+++ app/Models/Repairment.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Repairment extends Model
+{
+    use HasFactory;
+
+    protected $table = "repairment";
+    protected $primaryKey = "repariment_id";
+
+    protected $fillable = [
+        'repariment_id',
+        'description',
+        'request_date',
+        'repairment_date',
+        'price',
+        'staff_id',
+        'client_id',
+    ];
+}
