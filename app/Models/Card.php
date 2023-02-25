b0b34334919acc06f53910e1bbb92eb6cc08a2f8
Mayerly Bustos <bustosrobayom@gmail.com>
2023-02-25 13:32:19 -0500

    created model Card

---
+++ app/Models/Card.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Card extends Model
+{
+    use HasFactory;
+
+    protected $table = "card";
+    protected $primaryKey = "card_id";
+
+    protected $fillable = [
+        'card_id',
+        'card_number',
+        'type',
+        'cvv',
+        'expiration_date',
+        'user_client_id',
+    ];
+}
