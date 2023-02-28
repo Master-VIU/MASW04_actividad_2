5a3d45edc4bb5fe83b10b5d13a314779d75a7d1e
Mayerly Bustos <bustosrobayom@gmail.com>
2023-02-27 21:09:11 -0500

    creation of routes associated with Category

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::controller(CategoryController::class)->prefix('category')->group(function () {
+    Route::get('/', 'index');
+    Route::post('/', 'store');
+    Route::post('/{id}', 'update');
+    Route::put('/{id}', 'put');
+    Route::get('/{id}', 'show');
+    Route::delete('/{id}', 'destroy');
 });
