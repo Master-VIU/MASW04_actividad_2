5407db117fb288d25d24a2de1797868480fbc4d2
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-20 21:38:02 -0500

    Change user to users

--- app/Models/tmp/User.php
+++ app/Models/tmp/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $table = "user";
+    protected $table = "users";
     protected $primaryKey = "user_id";
     /**
      * The attributes that are mass assignable.
