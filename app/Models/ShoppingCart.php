368fdb1f7e70dc5b5cda13b7c9c9c1ea1c1906ea
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-21 17:21:12 -0500

    Fields defined in the model are deleted

--- app/Models/ShoppingCart.php
+++ app/Models/ShoppingCart.php
@@ -8,12 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class ShoppingCart extends Model
 {
     use HasFactory;
-
-    protected $table = "shopping_cart";
-    protected $primaryKey = "shopping_cart_id";
-
-    protected $fillable = [
-        'shopping_cart_id',
-        'total_cost',
-    ];
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,16 +9,4 @@ class User extends Model
 {
     use HasFactory;
 
-    protected $table = "users";
-    protected $primaryKey = "user_id";
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'username',
-        'password',
-    ];
-
 }
