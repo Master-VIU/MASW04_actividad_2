3ac223e3d89a32cc1942df7a1f8e30e5a6cc0e64
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-24 21:27:03 -0500

    Model fields are configured

--- app/Models/ShoppingCart.php
+++ app/Models/ShoppingCart.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class ShoppingCart extends Model
 {
     use HasFactory;
+
+    protected $table = 'shopping_cart';
+    protected $primaryKey = "shopping_cart_id";
+    /**
+     * The attributes that are mass assignablse.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'total_cost'     
+    ];
 }
