ee49bda5076e881e0eed06b9d41e86d5c2afe10d
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-26 12:26:17 -0500

    RequestValidateRating, Error message is created for validations

--- app/Http/Requests/rating/RequestValidateRating.php
+++ app/Http/Requests/rating/RequestValidateRating.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Requests\rating;
 
+use App\Models\ResultResponse;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
+
+use Illuminate\Contracts\Validation\Validator;
 
 class RequestValidateRating extends FormRequest
 {
@@ -15,5 +20,14 @@ class RequestValidateRating extends FormRequest
             'user_client_id' => 'required|numeric|exists:user_client,user_client_id',
             'product_id' => 'required|numeric|exists:product,product_id',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $resultResponse = new ResultResponse();
+        $resultResponse->setStatus(ResultResponse::UNPROCESSABLE_CONTENT);
+        $resultResponse->setData($validator->errors());
+        throw new HttpResponseException(response()->json($resultResponse,
+         Response::HTTP_UNPROCESSABLE_ENTITY));
     }
 }
