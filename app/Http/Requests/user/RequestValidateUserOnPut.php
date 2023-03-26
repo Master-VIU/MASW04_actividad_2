6ca7e8a80c85309ae2fe7b052593a8a03517ab30
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-26 12:25:11 -0500

    RequestValidateUserOnPut, Error message is created for validations

--- app/Http/Requests/user/RequestValidateUserOnPut.php
+++ app/Http/Requests/user/RequestValidateUserOnPut.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Requests\user;
 
+use App\Models\ResultResponse;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
 
 class RequestValidateUserOnPut extends FormRequest
 {
@@ -12,5 +16,14 @@ class RequestValidateUserOnPut extends FormRequest
             'username' => 'unique:user',
             'password' => 'min:8|max:100',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $resultResponse = new ResultResponse();
+        $resultResponse->setStatus(ResultResponse::UNPROCESSABLE_CONTENT);
+        $resultResponse->setData($validator->errors());
+        throw new HttpResponseException(response()->json($resultResponse,
+         Response::HTTP_UNPROCESSABLE_ENTITY));
     }
 }
