f5847bcc8151bd91b836e71f009ec900891e0156
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-26 12:28:58 -0500

    RequestValidateCard, Error message is added for validations

--- app/Http/Requests/card/RequestValidateCard.php
+++ app/Http/Requests/card/RequestValidateCard.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Requests\card;
 
+use App\Models\ResultResponse;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
+
+use Illuminate\Contracts\Validation\Validator;
 
 class RequestValidateCard extends FormRequest
 {
@@ -18,5 +23,14 @@ class RequestValidateCard extends FormRequest
             'expiration_date' => 'required|date',
             'user_client_id' => 'required|numeric|exists:user_client,user_client_id',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $resultResponse = new ResultResponse();
+        $resultResponse->setStatus(ResultResponse::UNPROCESSABLE_CONTENT);
+        $resultResponse->setData($validator->errors());
+        throw new HttpResponseException(response()->json($resultResponse,
+         Response::HTTP_UNPROCESSABLE_ENTITY));
     }
 }
