46a3c3e9ebd4e1767b4813e2fcc6c9e39eeb88a5
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-26 12:26:27 -0500

    RequestValidateProductImageOnPut, Error message is created for validations

--- app/Http/Requests/product_image/RequestValidateProductImageOnPut.php
+++ app/Http/Requests/product_image/RequestValidateProductImageOnPut.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Requests\product_image;
 
+use App\Models\ResultResponse;
 use Illuminate\Foundation\Http\FormRequest;
+
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
+
+use Illuminate\Contracts\Validation\Validator;
 
 class RequestValidateProductImageOnPut extends FormRequest
 {
@@ -12,5 +18,14 @@ class RequestValidateProductImageOnPut extends FormRequest
             'product_id' => 'numeric|exists:product,product_id',
             'image_path' => 'max:400',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $resultResponse = new ResultResponse();
+        $resultResponse->setStatus(ResultResponse::UNPROCESSABLE_CONTENT);
+        $resultResponse->setData($validator->errors());
+        throw new HttpResponseException(response()->json($resultResponse,
+         Response::HTTP_UNPROCESSABLE_ENTITY));
     }
 }
