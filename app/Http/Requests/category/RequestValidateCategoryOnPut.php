f7a078e154af3ffc292a6da84289e9283e26437d
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-26 12:28:24 -0500

    RequestValidateCategoryOnPut, Error message is added for validations

--- app/Http/Requests/category/RequestValidateCategoryOnPut.php
+++ app/Http/Requests/category/RequestValidateCategoryOnPut.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Requests\category;
 
+use App\Models\ResultResponse;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
+
+use Illuminate\Contracts\Validation\Validator;
 
 class RequestValidateCategoryOnPut extends FormRequest
 {
@@ -12,5 +17,14 @@ class RequestValidateCategoryOnPut extends FormRequest
             'name_category' => 'unique:category|max:250',
             'parent_category_id' => 'exists:category,category_id',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $resultResponse = new ResultResponse();
+        $resultResponse->setStatus(ResultResponse::UNPROCESSABLE_CONTENT);
+        $resultResponse->setData($validator->errors());
+        throw new HttpResponseException(response()->json($resultResponse,
+         Response::HTTP_UNPROCESSABLE_ENTITY));
     }
 }
