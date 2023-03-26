eab457b7014d9b7c6fe480449614ee66c6cca2a7
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-26 12:25:33 -0500

    RequestValidateShoppingCartProductOnPut, Error message is created for validations

--- app/Http/Requests/shopping_cart_product/RequestValidateShoppingCartProductOnPut.php
+++ app/Http/Requests/shopping_cart_product/RequestValidateShoppingCartProductOnPut.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Requests\shopping_cart_product;
 
+use App\Models\ResultResponse;
 use Illuminate\Foundation\Http\FormRequest;
+
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
+
+use Illuminate\Contracts\Validation\Validator;
 
 class RequestValidateShoppingCartProductOnPut extends FormRequest
 {
@@ -13,5 +19,14 @@ class RequestValidateShoppingCartProductOnPut extends FormRequest
             'product_id' => 'numeric|exists:product,product_id',
             'quantity' => 'numeric',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $resultResponse = new ResultResponse();
+        $resultResponse->setStatus(ResultResponse::UNPROCESSABLE_CONTENT);
+        $resultResponse->setData($validator->errors());
+        throw new HttpResponseException(response()->json($resultResponse,
+         Response::HTTP_UNPROCESSABLE_ENTITY));
     }
 }
