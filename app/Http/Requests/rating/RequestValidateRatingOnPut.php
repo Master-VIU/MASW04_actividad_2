7a0db94a5819f97102739d9a8ca14964164b8d59
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-26 12:26:07 -0500

    RequestValidateRatingOnPut, Error message is created for validations

--- app/Http/Requests/rating/RequestValidateRatingOnPut.php
+++ app/Http/Requests/rating/RequestValidateRatingOnPut.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Requests\rating;
 
+use App\Models\ResultResponse;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
+
+use Illuminate\Contracts\Validation\Validator;
 
 class RequestValidateRatingOnPut extends FormRequest
 {
@@ -15,5 +20,14 @@ class RequestValidateRatingOnPut extends FormRequest
             'user_client_id' => 'numeric|exists:user_client,user_client_id',
             'product_id' => 'numeric|exists:product,product_id',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $resultResponse = new ResultResponse();
+        $resultResponse->setStatus(ResultResponse::UNPROCESSABLE_CONTENT);
+        $resultResponse->setData($validator->errors());
+        throw new HttpResponseException(response()->json($resultResponse,
+         Response::HTTP_UNPROCESSABLE_ENTITY));
     }
 }
