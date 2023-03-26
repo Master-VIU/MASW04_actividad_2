94785ec2cc60ec6deb69a491e27f0266de9de1dd
Mayerly Bustos <bustosrobayom@gmail.com>
2023-03-26 12:27:23 -0500

    RequestValidatePersonOnPut, Error message is added for validations

--- app/Http/Requests/person/RequestValidatePersonOnPut.php
+++ app/Http/Requests/person/RequestValidatePersonOnPut.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Requests\person;
 
+use App\Models\ResultResponse;
 use Illuminate\Foundation\Http\FormRequest;
+
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
+
+use Illuminate\Contracts\Validation\Validator;
 
 class RequestValidatePersonOnPut extends FormRequest
 {
@@ -16,5 +22,14 @@ class RequestValidatePersonOnPut extends FormRequest
             'telephone' => 'min:9|max:50',
             'user_id' => 'numeric|exists:user,user_id'
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $resultResponse = new ResultResponse();
+        $resultResponse->setStatus(ResultResponse::UNPROCESSABLE_CONTENT);
+        $resultResponse->setData($validator->errors());
+        throw new HttpResponseException(response()->json($resultResponse,
+         Response::HTTP_UNPROCESSABLE_ENTITY));
     }
 }
